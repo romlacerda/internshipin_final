eec4448ffbdfacccb8a1b955016f8fae20defa0c
Romulo Lacerda <rlcrweb@gmail.com>
2017-09-27 00:19:13 -0300

    Estamos indo...

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_attached_file :documentation
+  validates_attachment :documentation, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907221528) do
+ActiveRecord::Schema.define(version: 20170920235055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20170907221528) do
     t.integer "status", limit: 2, default: 0
     t.datetime "datainicio"
     t.datetime "datafim"
+    t.string "documentation_file_name"
+    t.string "documentation_content_type"
+    t.integer "documentation_file_size"
+    t.datetime "documentation_updated_at"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
