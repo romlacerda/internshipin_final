b616d3c33e3dc7cfca7092da95d879c25400b70a
Romulo Lacerda <rlcrweb@gmail.com>
2017-10-28 06:58:48 -0200

    okokok

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,16 +26,8 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    @project.save
+    redirect_to projects_dashboard_path
   end
 
   def close
